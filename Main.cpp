4101650e95399d6330ba982e44edb66a975d612d
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2026-07-11 19:41:49 +0200

    Changed version to v1.1r1.

--- Main.cpp
+++ Main.cpp
@@ -68,7 +68,7 @@ struct File_s
 
 
 // ----- VARIABLES
-static constexpr char version[] = "sPostBuild v1.1rc1 ; " __DATE__ " " __TIME__; /**< @brief Application version. */
+static constexpr char version[] = "sPostBuild v1.1r1 ; " __DATE__ " " __TIME__; /**< @brief Application version. */
 
 static Input_s input; /**< @brief Input info from arguments. */
 static File_s fileInfo; /**< @brief Input file info. */
